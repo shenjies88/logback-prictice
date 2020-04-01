8e1be1d4d4c92bd293f625a8bb717c156c4d8fe1
shenjies88 <1961486254@qq.com>
2020-04-01 22:17:35 +0800

    first commit

---
+++ src/main/java/com/shenjies88/logback/practice/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.shenjies88.logback.practice;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author shenjies88
+ * @since 2020/4/1-5:35 PM
+ */
+class HelloWorld {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Logger logger = LoggerFactory.getLogger("com.shenjies88.logback.practice.HelloWorld");
+        int i = 10;
+        while (i > 0) {
+            logger.debug("Hello world {}",i--);
+            Thread.sleep(1000);
+        }
+    }
+}
